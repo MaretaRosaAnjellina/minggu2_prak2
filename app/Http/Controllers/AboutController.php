3b052d1f7526609f8a33e29bd7e607d158f07f42
MaretaRosaAnjellina <maretarossa_506@gmail.com>
2021-09-08 21:19:56 +0700

    modif2

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    //
+
+    public function about() {
+        return "NIM: 2031710006 <br> Nama: Mareta Rosa Anjellina";
+    }
+
+    
 }
